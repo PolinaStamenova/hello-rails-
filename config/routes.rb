112b824825c1eeaad839c91a8f27d12b70dd599d
PolinaStamenova <polina.stamenova@gmail.com>
2021-10-26 11:07:42 +0300

    Generate Pages controller #hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+ root  'pages#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
